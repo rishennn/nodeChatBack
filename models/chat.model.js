2953c893e8c3d545b99410d43487b71d882c5bd7
rishennn <143907382+rishennn@users.noreply.github.com>
2024-04-19 22:02:11 +0400

    123

--- models/chat.model.js
+++ models/chat.model.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const ChatSchema = mongoose.Schema({
-  idRoom: { type: String, required: true },
+  roomId: { type: String, required: true },
   name: { type: String, required: true },
   online: { type: Number, required: true, default: 0 },
   author: { type: Object, required: true },
